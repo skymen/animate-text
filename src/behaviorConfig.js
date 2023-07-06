23d91656ae2d9bc8887b1a82cbc3ebfd231f224f
Ossama <skymen75019@gmail.com>
2023-07-06 20:44:52 +0200

    upped version

--- src/behaviorConfig.js
+++ src/behaviorConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   addonType: "behavior",
   id: "skymen_Skymen_SpritefontDX",
   name: "Animate Text",
-  version: "1.1.0.1",
+  version: "1.1.0.2",
   category:
     // "attributes",
     // "movements",
